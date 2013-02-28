63103a3ec6f90e3f291e225a2029a774d2d43337
Jan Dockx <Jan_Dockx@peopleware.be>
2013-02-28 14:41:44 +0000

    mocks can now be reused with other super types (for persistence)
    git-svn-id: https://ppwcode.googlecode.com/svn/javascript/vernacular/semantics/trunk@6596 463e38ab-4f4e-0410-907a-65d2b3888964

--- src/semantics/ui/test/mock/Person.js
+++ src/semantics/ui/test/mock/Person.js
@@ -1,72 +1,7 @@
-define(["dojo/_base/declare", "../../../SemanticObject"],
-    function (declare, SemanticObject) {
+define(["./mockGenerator", "../../../SemanticObject"],
+    function (generator, SemanticObject) {
 
+      return generator.personClass(SemanticObject);
 
-      function nonEmptyStringOrNull(s) {
-        return (s && s !== "") ? s : null;
-      }
-
-      return declare([SemanticObject], {
-
-        _c_invar:[
-          function() {return this._c_prop_string("name")},
-          function() {return this._c_prop_string("street")},
-          function() {return this._c_prop_string("zip")},
-          function() {return this._c_prop_string("city")},
-          function() {return this._c_prop_string("tel")},
-          function() {return this._c_prop_string("email")}
-        ],
-
-        constructor:function (/*Object*/ props) {
-          this._c_pre(function() {return !props});
-          // TODO this should be removed when the superclass does it for us
-        },
-
-        name: null,
-        street: null,
-        zip: null,
-        city:null,
-        tel: null,
-        email: null,
-
-        wildExceptions: function() {
-          var cpe = [];// MUDO CompoundPropertyException
-          if (! this.get("name") || this.get("name") === "") {
-            cpe.push({source: this, propertyName: "name", message: "MANDATORY"});
-          }
-          return cpe;
-        },
-
-        reload: function(json) {
-          this._c_pre(function() { return (json !== null); });
-          this._c_pre(function() {return json.name && json.name !== null && json.name !== ""});
-
-          this.set("name", json.name);
-          this.set("street", nonEmptyStringOrNull(json.street));
-          this.set("zip", nonEmptyStringOrNull(json.zip));
-          this.set("city", nonEmptyStringOrNull(json.city));
-          this.set("tel", nonEmptyStringOrNull(json.tel));
-          this.set("email", nonEmptyStringOrNull(json.email));
-        },
-
-        _extendJsonObject:function (/*Object*/ json) {
-          json.name = this.name;
-          json.street = this.street;
-          json.zip = this.zip;
-          json.city = this.city;
-          json.tel = this.tel;
-          json.email = this.email;
-        },
-
-        _stateToString:function (/*Array of String*/ toStrings) {
-          toStrings.push("name: " + this.name);
-          toStrings.push("street: " + this.street);
-          toStrings.push("zip: " + this.zip);
-          toStrings.push("city: " + this.city);
-          toStrings.push("tel: " + this.tel);
-          toStrings.push("email: " + this.email);
-        }
-
-      });
     }
 );
--- src/semantics/ui/test/mock/SpecialPerson.js
+++ src/semantics/ui/test/mock/SpecialPerson.js
@@ -1,37 +1,7 @@
-define(["../../../../../dojo/_base/declare", "./Person"],
-    function (declare, Person) {
+define(["./mockGenerator", "./Person"],
+    function (generator, Person) {
 
-      function nonEmptyStringOrNull(s) {
-        return (s && s !== "") ? s : null;
-      }
+      return generator.specialPersonClass(Person);
 
-      return declare([Person], {
-
-        _c_invar:[
-          function() {return this._c_prop_string("email")}
-        ],
-
-        email: null,
-
-        reload: function(json) {
-          this._c_pre(function() { return (json !== null); });
-          this._c_pre(function() {return json.name && json.name !== null && json.name !== ""});
-
-          this.set("email", nonEmptyStringOrNull(json.email));
-        },
-
-        _extendJsonObject:function (/*Object*/ json) {
-          json.email = this.email;
-        },
-
-        _stateToString:function (/*Array of String*/ toStrings) {
-          toStrings.push("email: " + this.email);
-        },
-
-        isDeletable: function() {
-          return false;
-        }
-
-      });
     }
 );
