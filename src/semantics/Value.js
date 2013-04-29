f29a3bf473c5ac7dd1e3927292a41af884e79955
Jan Dockx <Jan_Dockx@peopleware.be>
2013-04-29 05:32:28 +0000

    given Value a once-over
    git-svn-id: https://ppwcode.googlecode.com/svn/javascript/vernacular/semantics/trunk@6782 463e38ab-4f4e-0410-907a-65d2b3888964

--- src/semantics/Value.js
+++ src/semantics/Value.js
@@ -1,29 +1,47 @@
-define(["dojo/_base/declare", "./PpwCodeObject", "ppwcode/contracts/_Mixin"],
-    function(declare, PpwCodeObject, _ContractMixin) {
+define(["dojo/_base/declare", "./PpwCodeObject", "ppwcode/contracts/_Mixin",
+        "ppwcode/oddsAndEnds/js"],
+    function(declare, PpwCodeObject, _ContractMixin,
+             js) {
 
-      return declare([PpwCodeObject, _ContractMixin], {
-// immutable
+      var Value = declare([PpwCodeObject, _ContractMixin], {
+        // summary:
+        //   Values are immutable after construction.
+        //   They are constructed with a kwargs argument.
+        //   For JSON-inification, toString, typeDescription, see PpwCodeObject.
 
         _c_invar: [
 
         ],
 
         constructor: function(/*Object*/ props) {
-          this._c_pre(function() {return props /* exists and not null */;}); // TODO and is an object
+          this._c_pre(function() {return props /* exists and not null */;});
+          this._c_pre(function() {return js.typeOf(props) === "object";});
 
           this._c_NOP(props);
         },
 
         equals: function(/*Value*/ other) {
-          this._c_ABSTRACT(other);
-          return false; // return boolean
+          // summary:
+          // description:
+          //   Must be overriden in subtypes with the pattern:
+          //   | return this.inherited(arguments) && (EXTRA CONDITIONS);
+
+          // Note: not a good candidate for chaining: look at what the overrider shoud write (it is the same)
+          this._c_pre(function() {
+            return other === null ||
+              (other && other.constructor && (other.constructor === this.constructor)); // same type
+          });
+
+          return true; // return boolean
         },
 
-        getValue: function() {
+        getValue: function() { // TODO what is this method for? Remove it?
           this._c_ABSTRACT();
           return null; // return object
         }
 
       });
+
+      return Value;
     }
 );
