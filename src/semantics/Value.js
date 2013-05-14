c20a1a1da40733840a1fd7eb5c92d52fd9bb4d04
Jan Dockx <Jan_Dockx@peopleware.be>
2013-05-14 13:21:57 +0000

    Value now has a natural order method
    git-svn-id: https://ppwcode.googlecode.com/svn/javascript/vernacular/semantics/trunk@6805 463e38ab-4f4e-0410-907a-65d2b3888964

--- src/semantics/Value.js
+++ src/semantics/Value.js
@@ -20,6 +20,31 @@ define(["dojo/_base/declare", "./PpwCodeObject", "ppwcode/contracts/_Mixin",
           this._c_NOP(props);
         },
 
+        compare: function(/*Value*/ other) {
+          // summary:
+          //   Return a negative number if this is considered smaller than other;
+          //   return a positive number if this is considered larger than other;
+          //   return 0 is this.equals(other)
+          // description:
+          //   This method should return consistent results: given a this and an other,
+          //   it should return the same result whenever the method is called, during
+          //   the entire live time of the object.
+          //   This extra requirement can be asked of Values, since they are immutable.
+          //   This method should realize a total order: it should be complete,
+          //   transitive, and anti-symmetric.
+          //
+          //   The pattern to express `this # other` is `this.compare(other) # 0`, with
+          //   # either <, =, or >.
+          //
+          //   In the order, undefined comes first and null comes second. However, it is impossible
+          //   to compare null and undefined, since neither can be used as the left-side in calling this method.
+          // tags:
+          //   public extension
+          this._c_pre(function() {return !other || (other.isInstanceOf && other.isInstanceOf(this.constructor));});
+
+          return this._c_ABSTRACT(); // return Number
+        },
+
         equals: function(/*Value*/ other) {
           // summary:
           // description:
@@ -28,12 +53,16 @@ define(["dojo/_base/declare", "./PpwCodeObject", "ppwcode/contracts/_Mixin",
           //   This implementation checks whether other is null.
           //
           //   This method is also used by dojox/mvc/sync! There can be no preconditions!
+          // tags:
+          //   public extension
 
           // Note: not a good candidate for chaining: look at what the overrider should write (it is the same)
 
-          return other && // return boolean
-            other.constructor &&
-            (other.constructor === this.constructor); // same type;
+          // we force to actually return a Boolean (!!)
+
+          return !!(other && // return boolean
+                    other.constructor &&
+                    (other.constructor === this.constructor)); // same type;
         },
 
         getValue: function() { // TODO what is this method for? Remove it?
