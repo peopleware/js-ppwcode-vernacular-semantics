8f9005fdf37d2b46342525b3f4b5f71787eadd6b
Jan Dockx <Jan_Dockx@peopleware.be>
2013-08-11 15:44:42 +0000

    * Cleanup of MultiLang capabilities over the entire project. ** new inheritance structure ** clean and clear update algorithm; it happened to work before, but it was entirely coincidental ** support for ${}$ evaluation in label strings
    git-svn-id: https://ppwcode.googlecode.com/svn/javascript/vernacular/semantics/trunk@6926 463e38ab-4f4e-0410-907a-65d2b3888964

--- src/semantics/ui/MultiLangValueOutput.js
+++ src/semantics/ui/MultiLangValueOutput.js
@@ -1,7 +1,7 @@
-define(["dojo/_base/declare", "ppwcode/oddsAndEnds/ui/_MultiLangOutput", "dojo/_base/lang"],
-  function(declare, _MultiLangOutput, lang) {
+define(["dojo/_base/declare", "ppwcode/oddsAndEnds/ui/MultiLangFormatOutput"],
+  function(declare, MultiLangFormatOutput) {
 
-    return declare([_MultiLangOutput], {
+    return declare([MultiLangFormatOutput], {
       // summary:
       //   This widget is a superclass for widgets that show (not-editable) the value of
       //   a Value in an i18n-ed way, and who can change the representation language.
@@ -12,48 +12,11 @@ define(["dojo/_base/declare", "ppwcode/oddsAndEnds/ui/_MultiLangOutput", "dojo/_
       //   The lang of this instance
       //   is injected as the locale in the options to that call, if no locale is set in formatOptions.
 
-      "-chains-": {
-        _extraFormatOption: "after"
-      },
-
-      // value: Value?
+      // value: Value
       value: null,
-      // re-render on set is already done in superclass
 
-      // formatOptions: Object
-      //   Passed as options when formatting `value`.
-      formatOptions: null,
-
-      _extraFormatOption: function(/*Object*/ options) {
-        // summary:
-        //   Subclasses can enforce extra properties on the options object
-        //   passed to this.value.constructor.format.
-        //   Chained.
-
-        // NOP
-      },
-
-      _output: function() {
-        var outputNode = this.srcNodeRef || this.domNode;
-        var result;
-        if (!this.value) {
-          result = this.get("missing");
-        }
-        else {
-          var opt = this.formatOptions ? lang.clone(this.formatOptions) : {};
-          opt.na = this.get("missing"); // overwrite
-          if (!opt.locale) { // formatOptions have precedence
-            opt.locale = this.findLang();
-          }
-          // default for escapeXml (where applicable) should be true; default is false in formatter
-          // so, we need to set it to true, if it was not explicitly set to false in the formatOptions
-          if (opt.escapeXml !== false) {
-            opt.escapeXml = true;
-          }
-          this._extraFormatOption(opt);
-          result = this.value.constructor.format(this.value, opt);
-        }
-        outputNode.innerHTML = result;
+      format: function(/*Value*/ value, /*Object*/ options) {
+        return value.constructor.format(value, options)
       }
 
     });
