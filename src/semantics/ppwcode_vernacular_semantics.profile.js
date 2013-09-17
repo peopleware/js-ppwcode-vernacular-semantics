d918e4f4bb669651a6daed655e8b691176f4405a
Mark Cuypers <Mark_Cuypers@peopleware.be>
2013-09-17 13:50:53 +0000

    Set the content of all package.json files to similar layout, version number, etc. Renamed all .profile.js files to the same naming convention including some package information in the name.
    git-svn-id: https://ppwcode.googlecode.com/svn/javascript/vernacular/semantics/trunk@7079 463e38ab-4f4e-0410-907a-65d2b3888964

---
+++ src/semantics/ppwcode_vernacular_semantics.profile.js
@@ -0,0 +1,29 @@
+var profile = (function () {
+  function isTest(filename, mid) {
+    return filename.indexOf("test/") >= 0;
+  }
+
+  function isCopyOnly(filename, mid) {
+    return filename.indexOf("nls/") >= 0;
+  }
+
+  function isAmd(filename, mid) {
+    return filename.indexOf(".json") < 0 && filename.indexOf(".js") >= 0 && filename.indexOf(".profile.js") < 0;
+  }
+
+  return {
+    resourceTags: {
+      test: function (filename, mid) {
+        return isTest(filename, mid);
+      },
+
+      copyOnly: function (filename, mid) {
+        return isCopyOnly(filename, mid);
+      },
+
+      amd: function (filename, mid) {
+        return !isTest(filename, mid) && !isCopyOnly(filename, mid) && isAmd(filename, mid);
+      }
+    }
+  };
+})();
